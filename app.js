2ee9916ce9facc2d451853fd6fb860a935248e1d
Banitongwa7 <davidbanitongwa2@yahoo.com>
2022-02-12 17:16:48 +0100

    [fix] error

--- app.js
+++ app.js
@@ -2,22 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const app = express();
+const indexRouters = require('./routes/index');
 
+app.set('view engine', 'pug');
 mongoose.connect('mongodb+srv://daviddb:Jean147258@cluster0.br7x1.mongodb.net/todoAppData?retryWrites=true&w=majority').then(() => {
     console.log('Connecter a la database');
 });
-
-
-
-
-const app = express();
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.set('view engine', 'pug');
-const indexRouters = require('./routes/index');
-app.use('/', indexRouters);
 app.use(methodOverride('_method'));
 
-const port = process.env.PORT;
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', indexRouters);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Serveur lancé sur le port ${port}`));
 
